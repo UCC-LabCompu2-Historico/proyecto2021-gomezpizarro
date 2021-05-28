0f73835014b48782c04ecf3cd7be0c759123032a
GonzaGomezPizarro <11912988@ucc.edu.ar>
2021-05-27 22:59:17 -0300

    arreglo loading

---
+++ carga.js
@@ -0,0 +1,10 @@
+// Uso un documento script separado para la pagina de carga por que estas funciones solo se pueden usar en dicha pagina
+
+function abrirweb (){
+    window.open('Jugador.html','_self');
+}
+
+async function  esperar (){
+    setTimeout(abrirweb,7997);
+}
+
