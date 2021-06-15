78b24b10ebd11881b078114b624cff1846192b45
GonzaGomezPizarro <11912988@ucc.edu.ar>
2021-06-15 08:14:06 -0300

    hola

--- FUNCIONES.js
+++ FUNCIONES.js
@@ -6,7 +6,7 @@
  * @param {Number} num3 //cifra 3
  * @param {Number} num4 //cifra 4
  */
-class numero { //anda correctamente
+class numero {
     constructor (num1,num2,num3,num4){
         this.num1=num1;
         this.num2=num2;
@@ -23,22 +23,11 @@ class numero { //anda correctamente
  * @param {Number} min - limite inferior
  * @return {Number} random entero
  */
-function random (min, max){ //anda correctamente
+function random (min, max){
     var num =  Math.floor((Math.random() * (max - min + 1)) + min);
     return (Math.round(num));
 }
-/**
- * congelar : congela el curso la de ejecucion por un determinado tiempo (usando un bucle que dura dicho tiempo)
- * @param {Number} segundos - tiempo a esperar en segundos
- * @return
- */
-function congelar (segundos) { //anda correctamente
-    let tiempo = new Date();
-    console.dir(tiempo);
-    tiempo.setSeconds(tiempo.getSeconds() + segundos);
-    while (new Date().getTime() < tiempo.getTime()) {
-    }
-}
+
 //crearemos una lista con todas las posibilidades de numeros de 4 cifras distintas;
 var lista = ["0123"];
 var i=124;
@@ -75,14 +64,14 @@ var canvas;
  * @param {string} id - lleva el id del imput donde se obtiene el num
  * @return
  */
-function obtener_num_intento_jugador(id) { //NO anda correctamente
+function obtener_num_intento_jugador(id) {
     console.log(intento_maquina.completo());//para poder el intento de la maquina en consola
     var num = document.getElementById(id).value;
     num=num.toString(); // para poder valuar que sea de 4 cifras
     var control = 0;
     canvas = document.getElementById('comentarios');
     var ctx = canvas.getContext('2d'); //preparamos el canvas
-    if ( num.length != 4){ //entra pero no ejecuta el contenido
+    if ( num.length != 4){
        alert ("EL NUMERO DEBE CONTENER 4 CIFRAS - Ingreselo de nuevo");
     }else{
         var aux1,aux2,aux3,aux4;
@@ -90,23 +79,23 @@ function obtener_num_intento_jugador(id) { //NO anda correctamente
         aux2=num[1];
         aux3=num[2];
         aux4=num[3];
-        if ( aux1==aux2 || aux1==aux3 || aux1==aux4 || aux2==aux3 || aux2==aux4 || aux3==aux4 ){ //entra pero no ejecuta el contenido
+        if ( aux1==aux2 || aux1==aux3 || aux1==aux4 || aux2==aux3 || aux2==aux4 || aux3==aux4 ){
             alert("RECURDE QUE NO SE DEBEN REPETIR CIFRAS");
         }else{
-            intento_jugador.num1 = Number(aux1);//no funciona
-            intento_jugador.num2 = Number(aux2);//no funciona
-            intento_jugador.num3 = Number(aux3);//no funciona
-            intento_jugador.num4 = Number(aux4);//no funciona
+            intento_jugador.num1 = Number(aux1);
+            intento_jugador.num2 = Number(aux2);
+            intento_jugador.num3 = Number(aux3);
+            intento_jugador.num4 = Number(aux4);
             control++;
             console.log(intento_jugador.completo());//para verlo en inspeccionar
         }
     }
-    if (control==1) { //entra pero no ejecuta el contenido
+    if (control==1) {
         intentos++;
         alert("INTENTO RECIBIDO");
-        document.getElementById('div1').style.display = 'none';//no funciona
+        document.getElementById('div1').style.display = 'none';
         var bien = 0, regular = 0;
-        document.getElementById('canvas').style.display = 'block';//no funciona
+        document.getElementById('canvas').style.display = 'block';
         if (intento_jugador.num1 == computadora.num1) bien++;
         if (intento_jugador.num2 == computadora.num2) bien++;
         if (intento_jugador.num3 == computadora.num3) bien++;
@@ -125,14 +114,15 @@ function obtener_num_intento_jugador(id) { //NO anda correctamente
         ctx.font="bold italic 20px arial";
         ctx.fillStyle = "brown";
         if ( intentos>12 ){
-            document.getElementById('comentarios').style.height = 500 + (30*(intentos-11));
+
+            document.getElementById('comentarios').style.height = 500 + (30*(intentos-12));
         }
         ctx.fillText("TUS INTENTOS", 40, 40 );
         ctx.fillText(texto, 40, 40 + (30 * intentos));
         document.getElementById('div5').style.display = 'block';
         document.getElementById('si_no').style.display = 'block';
         canvas = document.getElementById('adivinacionMaquina');
-        ctx = canvas.getContext('2d');//no funciona
+        ctx = canvas.getContext('2d');
         ctx.fillStyle = "brown";
         ctx.font="bold italic 35px arial";
         ctx.clearRect(20, 5, 150, 50);//para borrar
@@ -186,14 +176,14 @@ function REGULAR_BIEN(){
             //la lista de posibilidades se encuentra en string
             for (let i=0 ; i<lista.length ;i++) {
                 let cuenta_bien = 0 , cuenta_regular = 0;
-                if(intento_maquina.num1 == lista[i][0])cuenta_bien++;
-                if(intento_maquina.num2 == lista[i][1])cuenta_bien++;
-                if(intento_maquina.num3 == lista[i][2])cuenta_bien++;
-                if(intento_maquina.num4 == lista[i][3])cuenta_bien++;
-                if(intento_maquina.num1 == lista[i][1] || intento_maquina.num1 == lista[i][2] || intento_maquina.num1 == lista[i][3])cuenta_regular++;
-                if(intento_maquina.num2 == lista[i][0] || intento_maquina.num2 == lista[i][2] || intento_maquina.num2 == lista[i][3])cuenta_regular++;
-                if(intento_maquina.num3 == lista[i][0] || intento_maquina.num3 == lista[i][1] || intento_maquina.num3 == lista[i][3])cuenta_regular++;
-                if(intento_maquina.num4 == lista[i][0] || intento_maquina.num4 == lista[i][1] || intento_maquina.num4 == lista[i][2])cuenta_regular++;
+                if(intento_maquina.num1 === lista[i][0])cuenta_bien++;
+                if(intento_maquina.num2 === lista[i][1])cuenta_bien++;
+                if(intento_maquina.num3 === lista[i][2])cuenta_bien++;
+                if(intento_maquina.num4 === lista[i][3])cuenta_bien++;
+                if(intento_maquina.num1 === lista[i][1] || intento_maquina.num1 === lista[i][2] || intento_maquina.num1 === lista[i][3])cuenta_regular++;
+                if(intento_maquina.num2 === lista[i][0] || intento_maquina.num2 === lista[i][2] || intento_maquina.num2 === lista[i][3])cuenta_regular++;
+                if(intento_maquina.num3 === lista[i][0] || intento_maquina.num3 === lista[i][1] || intento_maquina.num3 === lista[i][3])cuenta_regular++;
+                if(intento_maquina.num4 === lista[i][0] || intento_maquina.num4 === lista[i][1] || intento_maquina.num4 === lista[i][2])cuenta_regular++;
                 if (cuenta_bien != bien || cuenta_regular != regular){
                     lista.splice(i,1);
                 }
